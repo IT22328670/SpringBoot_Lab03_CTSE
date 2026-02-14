122dedf3539cb94d2ea0361b46ae6f2e439d8e18
IT22328670 <it22328670@my.sliit.lk>
2026-02-15 01:25:36 +0530

    add the full updated code

--- src/main/java/com/sliit/product_service/model/Product.java
+++ src/main/java/com/sliit/product_service/model/Product.java
@@ -5,4 +5,44 @@ import jakarta.persistence.*;
 @Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+
+    //constructors
+    public Product() {}
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
